f71d91fabbfa869c8e22fc64025a2eebba33bd18
faisalkhalildev <sfaisalshah2024@gmail.com>
2026-02-18 22:51:54 +0500

    chnage simple

--- problem3.js
+++ problem3.js
@@ -2,7 +2,8 @@ const n = 253875;
 let temN = Math.abs(n)
 while (temN > 0){
  let digit = temN % 10;
- console.log('digit: ', digit);
+//  console.log('digit: ', digit);
 
- temN = Math.floor(temN / 10)
+//  temN = Math.floor(temN / 10)
+//  console.log(temN)
 }
