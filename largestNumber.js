b61ec7ccb568ecdf2e046c7e2cb543c28e6a216f
faisalkhalildev <sfaisalshah2024@gmail.com>
2026-05-06 17:23:54 +0500

    Successfully solve the largest number problem means how to solve the larest number

--- largestNumber.js
+++ largestNumber.js
@@ -1,7 +1,23 @@
-const numbers = [1,22,11,33,23,44,909,39,90];
-const largestNo = {}
+// const numbers = [1,22,11,33,23,44,909,39,90];
+// const largestNo = {}
 
-for(let i = 0; i < numbers.length; i++){
-    const total = numbers[i]
-    console.log(total)
+// for(let i = 0; i < numbers.length; i++){
+//     const total = numbers[i]
+//     // console.log(total)
+//     const lar = total
+//     if(lar >= total)
+
+// }
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,8,7,65,2222]
+let largestNo = numbers[0]
+for(let i = 0 ; i < numbers.length ; i++){
+    let currentNo = numbers[i]
+    if(currentNo > largestNo){
+        largestNo = currentNo;
+    } else {
+         largestNo = largestNo
+    }
 }
+console.log("The largest is: " , largestNo)
