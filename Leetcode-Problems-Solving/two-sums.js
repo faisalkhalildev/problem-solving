7a3ea1268c4b4002b4b19232da10d4a72e0b4462
faisalkhalildev <sfaisalshah2024@gmail.com>
2026-05-31 23:08:53 +0500

    solve the problem but i little bit remaining

---
+++ Leetcode-Problems-Solving/two-sums.js
@@ -0,0 +1,13 @@
+const nums = [2,7,11,15]
+const target = 9;
+let value = {}
+for(let i=0; i < nums.length; i++){
+    const current = nums[i]
+    // console.log(current)
+    if(current = 9){
+        console.log(current[i])
+    } else {
+      value = current;  
+    }
+}
+console.log(value)
